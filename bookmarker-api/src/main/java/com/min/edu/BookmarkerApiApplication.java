f05f651ef7c3e6107ffc7066071b08254c31f76a
lucas0429 <lucas0429@gmail.com>
2025-07-01 11:12:02 +0900

    feet: 내용 수정

--- bookmarker-api/src/main/java/com/min/edu/BookmarkerApiApplication.java
+++ bookmarker-api/src/main/java/com/min/edu/BookmarkerApiApplication.java
@@ -3,6 +3,10 @@ package com.min.edu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// github actions를 통해서 CI를 구축했습니다
+// CI를 통해서 자동으로  변경이 발생하면 Docker hub에 이미지를 자동으로 배포 합니다
+
+
 @SpringBootApplication
 public class BookmarkerApiApplication {
 
